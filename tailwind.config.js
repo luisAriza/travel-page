a3ff89533bfb3858d306f10440a4cfd9f1138824
Luis Ariza <luismanuelariza@hotmail.com>
2022-06-16 09:50:16 -0500

    Carpeta de distribucion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  content: ["./dist/index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       backgroundImage: {
-        sanFrancisco: "url('./assets/img/sanFrancisco.jpg')",
-        sanFranciscoDesktop: "url('./assets/img/sanFranciscoDesktop.jpg')",
-        bail: "url('./assets/img/bail.jpg')",
-        chicago: "url('./assets/img/chicago.jpg')",
-        europe: "url('./assets/img/europe.jpg')",
-        iceland: "url('./assets/img/iceland.jpg')",
-        LA: "url('./assets/img/LA.jpg')",
-        miami: "url('./assets/img/miami.jpg')",
-        new_york: "url('./assets/img/new_york.jpg')",
-        norway: "url('./assets/img/norway.jpg')",
-        seattle: "url('./assets/img/seattle.jpg')",
-        switzerland: "url('./assets/img/switzerland.jpg')",
-        sydney: "url('./assets/img/sydney.jpg')",
-        yosemite: "url('./assets/img/yosemite.jpg')",
+        sanFrancisco: "url('../assets/img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../assets/img/sanFranciscoDesktop.jpg')",
+        bail: "url('../assets/img/bail.jpg')",
+        chicago: "url('../assets/img/chicago.jpg')",
+        europe: "url('../assets/img/europe.jpg')",
+        iceland: "url('../assets/img/iceland.jpg')",
+        LA: "url('../assets/img/LA.jpg')",
+        miami: "url('../assets/img/miami.jpg')",
+        new_york: "url('../assets/img/new_york.jpg')",
+        norway: "url('../assets/img/norway.jpg')",
+        seattle: "url('../assets/img/seattle.jpg')",
+        switzerland: "url('../assets/img/switzerland.jpg')",
+        sydney: "url('../assets/img/sydney.jpg')",
+        yosemite: "url('../assets/img/yosemite.jpg')",
       },
       backgroundColor: (theme) => ({
         ...theme("colors"),
