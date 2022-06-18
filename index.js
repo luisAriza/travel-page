df7eb4d91afda537035c9cfc01af04df2ab0fe1b
Luis Ariza <luismanuelariza@hotmail.com>
2022-06-18 18:43:36 -0500

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const htmlElement = document.querySelector("html");
 const toggleButton = document.querySelector("#toggle");
+const toggleButtonTabBar = document.querySelector("#toggleTabBar");
 
 toggleButton.addEventListener("click", () => {
+  htmlElement.classList.toggle("dark");
+});
+toggleButtonTabBar.addEventListener("click", () => {
   htmlElement.classList.toggle("dark");
 });
