77276a7e03c1eafe07bb66914734f87f1be6979c
Luis Ariza <luismanuelariza@hotmail.com>
2022-06-19 19:42:38 -0500

    Updates

---
+++ index.f13b7a29.js
@@ -0,0 +1,2 @@
+const htmlElement=document.querySelector("html"),toggleButton=document.querySelector("#toggle"),toggleButtonTabBar=document.querySelector("#toggleTabBar");toggleButton.addEventListener("click",(()=>{htmlElement.classList.toggle("dark")})),toggleButtonTabBar.addEventListener("click",(()=>{htmlElement.classList.toggle("dark")}));
+//# sourceMappingURL=index.f13b7a29.js.map
