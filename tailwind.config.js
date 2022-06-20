da01e02f4ba882912298650d64a891632cb1872d
Luis Ariza <luismanuelariza@hotmail.com>
2022-06-20 17:43:59 -0500

    Identacion de codigo css del header y footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{pug,html,js,ts,jsx,tsx}"],
+  content: ["./dist/**/*.html", "./src/**/*.{pug,html,js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
