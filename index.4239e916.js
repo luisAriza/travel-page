59df0397a62037b2f58114bd92f9c5407c54f4b1
Luis Ariza <luismanuelariza@hotmail.com>
2022-06-20 10:11:44 -0500

    Updates

--- index.4239e916.js
+++ index.4239e916.js
@@ -1,2 +1,2 @@
 const e=document.querySelector("html"),t=document.querySelector("#toggle"),c=document.querySelector("#toggleTabBar");t.addEventListener("click",(()=>{e.classList.toggle("dark")})),c.addEventListener("click",(()=>{e.classList.toggle("dark")}));
-//# sourceMappingURL=index.f1499327.js.map
+//# sourceMappingURL=index.4239e916.js.map
